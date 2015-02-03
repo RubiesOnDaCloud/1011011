6445181f6c83caa11a6440797cff431834f6b7d0
Jalyna <jalyna.schroeder@dawandamail.com>
2015-02-03 20:54:37 +0100

    Create fullname.rb

---
+++ fullname.rb
@@ -0,0 +1,7 @@
+puts 'what is your firstname?'
+firstname = gets.chomp
+puts 'what is your middlename?'
+middlename = gets.chomp
+puts 'what is your lastname?'
+lastname = gets.chomp
+puts 'Hello, ' + firstname + ' ' + middlename + ' ' + lastname
